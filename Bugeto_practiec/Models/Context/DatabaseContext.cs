415eb735876f9f78a0846d7df2e4ddc86679780e
ACER <ACER@DESKTOP-AO608KQ>
2025-10-24 21:41:35 +0330

    Add project files.

---
+++ Bugeto_practiec/Models/Context/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Bugeto_practiec.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bugeto_practiec.Models.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    
+
+        public DbSet<Customer> Customers { get; set; }
+
+        
+    }
+}
---
+++ Bugeto_practiec/Models/Entity/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Bugeto_practiec.Models.Entity
+{
+    public class Customer
+    {
+        public long CostomerId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
