b3ceed6d0df9fccb928d5d112cab2b2481b99bf3
ACER <ACER@DESKTOP-AO608KQ>
2025-10-25 22:32:29 +0330

    Add initial migration for Identity and User model

---
+++ Bugeto_practiec/Migrations/20251025184315_Init.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bugeto_practiec.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
--- Bugeto_practiec/Models/Entity/Customer.cs
+++ Bugeto_practiec/Models/Entity/Customer.cs
@@ -2,7 +2,7 @@
 {
     public class Customer
     {
-        public long CostomerId { get; set; }
+        public long CustomerId { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
 
